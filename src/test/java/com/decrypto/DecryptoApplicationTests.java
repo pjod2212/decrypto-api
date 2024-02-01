3bc23ce04506acf1a56c8d19db78c6f6a0613d2f
pdonati <pdonati@reba.com.ar>
2024-02-01 20:08:23 -0300

    subo proyecto

---
+++ src/test/java/com/decrypto/DecryptoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.decrypto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecryptoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
